0706c77b15d2a467567fb6db6be5f96b8595e1d5
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-08-07 18:08:02 -0600

    updated header to bootstrap

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,12 +3,14 @@ import React from "react";
 class Header extends React.Component {
     render () {
         return (
-            <navbar className="header">
-                This is the header
-                <div><a>link 1</a></div>
-                <div><a>link 2</a></div>
-                <div><a>link 3</a></div>
-            </navbar>
+            <nav class="navbar-inverse">
+                <ul class="nav">
+                    <li role="presentation"><a href=''>Home</a></li>
+                    <li role="presentation"><a href=''>Trip Planner</a></li>
+                    <li role="presentation"><a href=''>My trips</a></li>
+                    <li role="presentation"><a href=''>Weather</a></li>
+                </ul>
+            </nav>
         )
     }
 }
