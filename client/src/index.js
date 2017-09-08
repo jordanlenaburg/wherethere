ec353b2a181730540767c467ce87f4bf0ec19a52
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-09-08 17:31:23 -0600

    added login page component

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import Header from "./components/header"
+import LoginPage from "./components/login"
 
 class App extends React.Component {
     render() {
         return (
             <div className="container">
-                 <Header />
-                <h1>test</h1>
+                <Header />
+                <h1>Where, there?</h1>
+                <LoginPage />
             </div>
         )
     }
